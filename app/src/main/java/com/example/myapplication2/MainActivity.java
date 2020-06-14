7a04ee0e65bde3960b3c8f74a3a343f1a75745a6
NicoCia <nicocia7@gmail.com>
2020-06-14 02:21:42 -0300

    MainActivity con Fragmento

--- app/src/main/java/com/example/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -3,10 +3,14 @@ package com.example.myapplication2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.myapplication2.ui.vista.VistaFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    VistaFragment vistaFragment = new VistaFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,8 +18,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.main_activity);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.container, VistaFragment.newInstance())
+                    .replace(R.id.container, vistaFragment)
                     .commitNow();
         }
     }
+
+    public void onClick(View view){
+        TextView textView = (TextView) findViewById(R.id.message);
+        textView.setText(R.string.texto_prueba);
+    }
+
 }
--- app/src/main/java/com/example/myapplication2/ui/vista/VistaFragment.java
+++ app/src/main/java/com/example/myapplication2/ui/vista/VistaFragment.java
@@ -11,12 +11,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.myapplication2.R;
 
 public class VistaFragment extends Fragment {
 
     private VistaViewModel mViewModel;
+    private View myInflatedView;
 
     public static VistaFragment newInstance() {
         return new VistaFragment();
@@ -26,14 +28,14 @@ public class VistaFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.vista_fragment, container, false);
+        this.myInflatedView = inflater.inflate(R.layout.vista_fragment, container, false);
+        return myInflatedView;
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         mViewModel = ViewModelProviders.of(this).get(VistaViewModel.class);
-        // TODO: Use the ViewModel
     }
 
 }
