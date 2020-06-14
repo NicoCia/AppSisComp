df1ba39de110fc9e1a9043a7c07ec34ede536135
NicoCasas <nicolas.casas.250@gmail.com>
2020-06-14 01:24:17 -0300

    Comentario sobre updateModel

--- app/src/main/java/com/example/myapplication2/Modelo.java
+++ app/src/main/java/com/example/myapplication2/Modelo.java
@@ -17,6 +17,16 @@ public class Modelo {
         alarma=false;
     }
 
+    /*  Recibe un string y actualiza las variables asociadas, controlando si los valores se
+        encuentran en los limites permitidos.
+        Los formatos de mensaje válidos son:
+            - temperatura xx humedad xx
+            - temperaturaMAX xx
+            - humedadMIN xx
+        Si los valores de temperatura y humedad están fuera de rango, se notifica al controlador y
+        se setea alarma en 1.
+
+     */
     public void updateModel(String cadena){
         //substring(int beginIndex, int endIndex) temperatura xx humedad xx -> 25 de largo
         if(cadena.length()==25) {
