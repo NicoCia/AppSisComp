133fa87e603dd6f03a8b4e7fc834a55ba127a36c
NicoCasas <nicolas.casas.250@gmail.com>
2020-06-13 21:12:31 -0300

    Modelo

---
+++ app/src/main/java/com/example/myapplication2/Modelo.java
@@ -0,0 +1,67 @@
+package com.example.myapplication2;
+
+public class Modelo {
+    private int temperatura;
+    private int temperaturaMAX;
+    private int humedad;
+    private int humedadMIN;
+    private int tiempo;
+    private boolean alarma;
+
+    public Modelo(){
+        temperatura=0;
+        temperaturaMAX=60;
+        humedad=40;
+        humedadMIN=30;
+        tiempo=0;
+        alarma=false;
+    }
+
+    public int getTemperatura() {
+        return temperatura;
+    }
+
+    public void setTemperatura(int temperatura) {
+        this.temperatura = temperatura;
+    }
+
+    public int getTemperaturaMAX() {
+        return temperaturaMAX;
+    }
+
+    public void setTemperaturaMAX(int temperaturaMAX) {
+        this.temperaturaMAX = temperaturaMAX;
+    }
+
+    public int getHumedad() {
+        return humedad;
+    }
+
+    public void setHumedad(int humedad) {
+        this.humedad = humedad;
+    }
+
+    public int getHumedadMIN() {
+        return humedadMIN;
+    }
+
+    public void setHumedadMIN(int humedadMIN) {
+        this.humedadMIN = humedadMIN;
+    }
+
+    public int getTiempo() {
+        return tiempo;
+    }
+
+    public void setTiempo(int tiempo) {
+        this.tiempo = tiempo;
+    }
+
+    public boolean isAlarma() {
+        return alarma;
+    }
+
+    public void setAlarma(boolean alarma) {
+        this.alarma = alarma;
+    }
+}
